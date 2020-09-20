0025fa5051127f23a4342608a817d48b8d348f23
Gowshik Prabhu <gowshikprabhu@gmail.com>
2020-09-20 22:36:38 +0530

    Create home page with buttons

--- lib/home.dart
+++ lib/home.dart
@@ -6,8 +6,79 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  bool _loading = true;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Color(0xFF101010),
+      body: Container(
+        padding: EdgeInsets.symmetric(horizontal: 24),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            SizedBox(height: 80),
+            Text(
+              'Predict Rock Paper Scissor by CNN',
+              style: TextStyle(fontSize: 17, color: Colors.blueAccent),
+            ),
+            SizedBox(height: 50),
+            Center(
+              child: _loading
+                  ? Container(
+                      width: 250,
+                      child: Column(
+                        children: <Widget>[
+                          Image.asset('assets/img-1.png'),
+                          SizedBox(height: 50)
+                        ],
+                      ),
+                    )
+                  : Container(),
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width,
+              child: Column(children: <Widget>[
+                GestureDetector(
+                  onTap: () {},
+                  child: Container(
+                    width: MediaQuery.of(context).size.width - 100,
+                    alignment: Alignment.center,
+                    padding: EdgeInsets.symmetric(horizontal: 24, vertical: 17),
+                    decoration: BoxDecoration(
+                      color: Colors.blueAccent,
+                      borderRadius: BorderRadius.circular(6),
+                    ),
+                    child: Text(
+                      'Take a photo',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                GestureDetector(
+                  onTap: () {},
+                  child: Container(
+                    width: MediaQuery.of(context).size.width - 100,
+                    alignment: Alignment.center,
+                    padding: EdgeInsets.symmetric(horizontal: 24, vertical: 17),
+                    decoration: BoxDecoration(
+                      color: Colors.blueAccent,
+                      borderRadius: BorderRadius.circular(6),
+                    ),
+                    child: Text(
+                      'Camera Roll',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ),
+              ]),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
