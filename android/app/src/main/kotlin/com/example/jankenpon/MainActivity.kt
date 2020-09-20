a53dffacce70fc9c5614177949b189ca50b334ff
Gowshik Prabhu <gowshikprabhu@gmail.com>
2020-09-20 20:01:03 +0530

    Initial setup

---
+++ android/app/src/main/kotlin/com/example/jankenpon/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.jankenpon
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Jan Ken Pon',
+      home: MyHome(),
+    );
+  }
+}
+
+class MyHome extends StatefulWidget {
+  @override
+  _MyHomeState createState() => _MyHomeState();
+}
+
+class _MyHomeState extends State<MyHome> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('Hello'),
+    );
+  }
+}
