03e6c7be3989a15c9f6bce7e900d004423581f69
Gowshik Prabhu <gowshikprabhu@gmail.com>
2020-09-20 20:18:35 +0530

    add splash to main file

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:jankenpon/splashscreen.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,21 +11,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Jan Ken Pon',
-      home: MyHome(),
-    );
-  }
-}
-
-class MyHome extends StatefulWidget {
-  @override
-  _MyHomeState createState() => _MyHomeState();
-}
-
-class _MyHomeState extends State<MyHome> {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: Text('Hello'),
+      home: MySplashScreen(),
     );
   }
 }
